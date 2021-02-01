e4e7134afb6769e3e6890faa6f6de7ba71832629
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-02-01 15:06:49 -0500

    test

--- trackr/src/components/RegisterForm.js
+++ trackr/src/components/RegisterForm.js
@@ -3,3 +3,9 @@
 // password
 // confirmpassword:
 // Location:        (optional) (placeholder text: enter zip code, address, GPS coordinates, or leave blank for now! )
+
+export default function RegisterForm () {
+    return (
+        <div>this is a test</div>
+    );
+}
