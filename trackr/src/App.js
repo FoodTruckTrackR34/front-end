d987be1fd65ca31aea593601ae9fb711cfab21ea
Michael Murphy <mmurph917@michaels-mbp.lan>
2021-02-02 13:15:37 -0600

    DinerDashboard functionally almost complete

--- trackr/src/App.js
+++ trackr/src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import RegisterForm from "./components/RegisterForm";
+import DinerDash from "./components/DinerDash"
+import OperatorDash from "./components/OperatorDash"
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import "./App.css";
+
+const initialUsers = [];
+
+const initialDinerFormValues = {
+  dinerUsername: "",
+  dinerEmail: "",
+  dinerPassword: "",
+  dinerConfirmPassword: "",
+  dinerZipcode: "",
+  type: "",
+};
+
+const initialOperatorFormValues = {
+  operatorUsername: "",
+  operatorEmail: "",
+  operatorPassword: "",
+  operatorConfirmPassword: "",
+  type: "",
+};
+
+const initialDinerDisabled = false;
+const initialOperatorDisabled = false;
 
 function App() {
+  const [users, setUsers] = useState(initialUsers);
+  const [dinerFormValues, setDinerFormValues] = useState(
+    initialDinerFormValues
+  );
+  const [operatorFormValues, setOperatorFormValues] = useState(
+    initialOperatorFormValues
+  );
+  const [dinerButton, setDinerButton] = useState(initialDinerDisabled);
+  const [operatorButton, setOperatorButton] = useState(initialOperatorDisabled);
+
+  const dinerInputChange = (name, value) => {
+    setDinerFormValues({
+      ...dinerFormValues,
+      [name]: value,
+    });
+  };
+
+  const operatorInputChange = (name, value) => {
+    setOperatorFormValues({
+      ...operatorFormValues,
+      [name]: value,
+    });
+  };
+
+  const dinerFormSubmit = () => {
+    const newDiner = {
+      dinerUsername: dinerFormValues.dinerUsername.trim(),
+      dinerEmail: dinerFormValues.dinerEmail.trim(),
+      dinerPassword: dinerFormValues.dinerPassword.trim(),
+      dinerConfirmPassword: dinerFormValues.dinerConfirmPassword.trim(),
+      dinerZipcode: dinerFormValues.dinerZipcode.trim(),
+      type: "diner",
+    };
+
+    setUsers([...users, newDiner]);
+    setDinerFormValues(initialDinerFormValues);
+    // postNewDiner(newDiner);
+  };
+
+  // const postNewDiner = (newDiner) => {
+  //   axios
+  //   .post('https://reqres.in/api/users', newDiner)
+  //   .then((res) => {
+  //     setUsers([...users, res.data]);
+  //     setDinerFormValues(initialDinerFormValues);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   })
+  // }
+
+  const operatorFormSubmit = () => {
+    const newOperator = {
+      operatorUsername: operatorFormValues.operatorUsername.trim(),
+      operatorEmail: operatorFormValues.operatorEmail.trim(),
+      operatorPassword: operatorFormValues.operatorPassword.trim(),
+      operatorConfirmPassword: operatorFormValues.operatorConfirmPassword.trim(),
+      type: "operator",
+    };
+
+    setUsers([...users, newOperator]);
+    setOperatorFormValues(initialOperatorFormValues);
+    // postNewOperator(newOperator);
+  };
+
+  // const postNewOperator = (newOperator) => {
+  //   axios
+  //   .post('https://reqres.in/api/users', newOperator)
+  //   .then((res) => {
+  //     setUsers([...users, res.data]);
+  //     setOperatorFormValues(initialOperatorFormValues);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   })
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <RegisterForm
+        dinerChange={dinerInputChange}
+        dinerDisabled={dinerButton}
+        dinerFormSubmit={dinerFormSubmit}
+        dinerValues={dinerFormValues}
+        operatorChange={operatorInputChange}
+        operatorDisabled={operatorButton}
+        operatorFormSubmit={operatorFormSubmit}
+        operatorValues={operatorFormValues}
+      />
+      <DinerDash />
+      <OperatorDash /> */
+      <Footer />
     </div>
   );
 }
