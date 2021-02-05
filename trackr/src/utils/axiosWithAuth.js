532b3c7721e5cb72805cebbfa9e3981ca7442780
Joshua Lovins <joshualovins@Joshuas-MBP.lan>
2021-02-05 16:47:00 -0500

    axiosWithAuth fixed

--- trackr/src/utils/axiosWithAuth.js
+++ trackr/src/utils/axiosWithAuth.js
@@ -1,10 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
-const authConfig = {
-  headers: { Authorization: localStorage.getItem("token") },
-  baseURL: "https://food-truck-back-end-lambda.herokuapp.com", //need to get the api address to attach here for base url setup
-};
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem('token');
 
-export const axiosWithAuth = (config = authConfig) => {
-  return axios.create(config);
-};
+    return axios.create({
+        baseURL: "https://food-truck-back-end-lambda.herokuapp.com",
+        headers: {
+            Authorization: token
+        }
+    })
+}
